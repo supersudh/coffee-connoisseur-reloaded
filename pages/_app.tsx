95e0d98856c44b77a190680adb7fe13c8fd5c3bb
Sudharsan RaviKumar <supersudh@gmail.com>
2022-12-23 14:32:19 +0530

    1. Add gradient background 2. Remove stock/boilerplate styles 3. Organize Home component and styles
    todo: Next commit, pending styles and CTAs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,12 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />{" "}
+      <footer>
+        <p>© {new Date().getFullYear()} supersudh</p>
+    </footer>
+    </div>
+  );
 }
